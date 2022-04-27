df461d59ef2fe7ed9a4c2e48ee84c6ce0ed0ba13
Maksim Karpets <maksimkarpets@mail.ru>
2022-04-27 21:58:57 +0300

    change creds

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-  "dfae1ns8mq9vi9",
-  "tujhrbziacqqhe",
-  "47aef6d908b4cb2d993a75c5ca39353a79b8210ef311ed592852f4c204cab4c5",
+  "dcbni6chbb6dh9",
+  "kphfixicsbnmjk",
+  "cd54a9562bb02d00e129caf05ce2707f1c8e6201a357ba588e61e5c6d3ca92b6",
   {
-    host: "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com",
+    host: "ec2-63-35-156-160.eu-west-1.compute.amazonaws.com",
     port: "5432",
     dialect: "postgres",
     dialectOptions: {
